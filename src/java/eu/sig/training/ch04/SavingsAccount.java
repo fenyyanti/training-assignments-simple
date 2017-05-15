849b1a0e180e4080f9e12c930630f18ef6ef61dd
fenyyanti <javana.utphala@gmail.com>
2017-05-15 23:31:51 +0700

    SavingAccount: deduplicate

--- src/java/eu/sig/training/ch04/SavingsAccount.java
+++ src/java/eu/sig/training/ch04/SavingsAccount.java
@@ -8,17 +8,12 @@ public class SavingsAccount {
 
     public Transfer makeTransfer(String counterAccount, Money amount) 
         throws BusinessException {
-        // 1. Assuming result is 9-digit bank account number, validate 11-test:
-        int sum = 0; // <1>
-        for (int i = 0; i < counterAccount.length(); i++) {
-            char character = counterAccount.charAt(i);
-            int characterValue = Character.getNumericValue(character);
-            sum = sum + (9 - i) * characterValue;
-        }
-        if (sum % 11 == 0) {
+    	
+    	if (isValidAcct(counterAccount))
+        {
             // 2. Look up counter account and make transfer object:
             CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
-            Transfer result = new Transfer(this, acct, amount); // <2>
+            Transfer result = new Transfer(this, acct, amount); // <2>       
             // 3. Check whether withdrawal is to registered counter account:
             if (result.getCounterAccount().equals(this.registeredCounterAccount)) 
             {
@@ -30,6 +25,17 @@ public class SavingsAccount {
             throw new BusinessException("Invalid account number!!");
         }
     }
+    
+    
+    public static boolean isValidAcct(String counterAccount) {
+    	 // 1. Assuming result is 9-digit bank account number, validate 11-test:
+        int sum = 0; // <1>
+        for (int i = 0; i < counterAccount.length(); i++) {
+            sum = sum + (9 - i) * Character.getNumericValue(counterAccount.charAt(i));
+        }
+        return sum % 11 == 0;
+    }
+    
 
     public void addInterest() {
         Money interest = balance.multiply(INTEREST_PERCENTAGE);
