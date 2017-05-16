3f9c32837f90ea2817405d48cc4dabee7c239827
fenyyanti <javana.utphala@gmail.com>
2017-05-16 11:55:09 +0700

    Commit remove unused method

--- src/java/eu/sig/training/ch04/Accounts.java
+++ src/java/eu/sig/training/ch04/Accounts.java
@@ -6,13 +6,4 @@ public class Accounts {
         return new CheckingAccount();
     }
 
-    // tag::isValid[]
-    public static boolean isValid(String number) {
-        int sum = 0;
-        for (int i = 0; i < number.length(); i++) {
-            sum = sum + (9 - i) * Character.getNumericValue(number.charAt(i));
-        }
-        return sum % 11 == 0;
-    }
-    // end::isValid[]
 }
