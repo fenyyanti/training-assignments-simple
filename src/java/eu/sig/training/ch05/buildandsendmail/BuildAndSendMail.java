b0eab4ced5462fb91a42b8e36f1347d46d82bd54
fenyyanti <javana.utphala@gmail.com>
2017-05-16 16:50:32 +0700

    Test Commit Extract Parameter

--- src/java/eu/sig/training/ch05/buildandsendmail/BuildAndSendMail.java
+++ src/java/eu/sig/training/ch05/buildandsendmail/BuildAndSendMail.java
@@ -2,12 +2,10 @@ package eu.sig.training.ch05.buildandsendmail;
 
 public class BuildAndSendMail {
     // tag::buildAndSendMail[]
-    public void buildAndSendMail(MailMan m, String firstName, String lastName,
-        String division, String subject, MailFont font, String message1,
+    public void buildAndSendMail(MailMan m, String employeeName, String subject, MailFont font, String message1,
         String message2, String message3) {
         // Format the email address
-        String mId = firstName.charAt(0) + "." + lastName.substring(0, 7) + "@"
-            + division.substring(0, 5) + ".compa.ny";
+        String mId = employeeName + ".compa.ny";
         // Format the message given the content type and raw message
         MailMessage mMessage = formatMessage(font,
             message1 + message2 + message3);
